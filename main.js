2c1245792d9e48c5f08097dd1aec7129e19d5e89
ibezimvivian <ibezimngozi2019@gmail.com>
2023-04-19 17:36:02 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,30 @@
+const text = document.querySelector('.text')
+const btn_1 = document.querySelector('.btn-1')
+const btn_2 = document.querySelector('.btn-2')
+const btn_3 = document.querySelector('.btn-3')
+const bodyCnt = document.querySelector('body')
+
+function colorSwitcher(caption,color, opec) {
+     text.innerHTML = caption
+     bodyCnt.style.backgroundColor = color
+     text.style.opacity = opec
+}
+
+
+btn_1.addEventListener('click', ()=>{
+    colorSwitcher('YOU MEAN THE WORLD TO ME', '#ea1d19', '1')
+    
+   
+})
+
+btn_2.addEventListener('click', ()=>{
+    colorSwitcher('YOU ARE BEAUTIFUL', '#f7f70c')
+    
+   
+})
+
+btn_3.addEventListener('click', ()=>{
+    colorSwitcher('I LOVE YOU EVERYDAY', '#2807ba')
+    
+   
+})
